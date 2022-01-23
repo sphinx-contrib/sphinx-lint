3ce424dc6ba23774bebaaab27aee6dbe91f58ce9
Julien Palard <julien@palard.fr>
2022-01-23 22:22:50 +0100

    Spotted a bug when a file ends with no newline.

--- tests/test_nonregression.py
+++ tests/test_nonregression.py
@@ -9,5 +9,20 @@ def test_role_missing_column(capsys, tmp_path):
     error_count = main(["rstlint.py", str(tmp_path)])
     out, err = capsys.readouterr()
     assert "column" in out
-    assert err == ""
+    assert not err
     assert error_count == 1
+
+
+@pytest.mark.xfail(strict=True)
+def test_last_line(capsys, tmp_path):
+    """Check regression of last line ending with space, a char, and no newline.
+
+    It wrongly raised a "trailing whitespace".
+    """
+
+    file = tmp_path / "testfile.rst"
+    file.write_text("Hell o", encoding="UTF-8")
+    main(["rstlint.py", str(tmp_path)])
+    out, err = capsys.readouterr()
+    assert not err
+    assert "trailing whitespace" not in out
