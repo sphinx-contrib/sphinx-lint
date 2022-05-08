c9e4b54db407536fca09ccdc847d6b6630ab1517
Julien Palard <julien@palard.fr>
2022-05-08 21:19:41 +0200

    More accurate bullet counting.

--- tests/test_enable_disable.py
+++ tests/test_enable_disable.py
@@ -1,20 +1,25 @@
 from random import choice
+import re
 
 from sphinxlint import main
 
-BULLET = "- "
+BULLET = re.compile(r"^\s*- ", flags=re.MULTILINE)
+
+
+def count_bullets(text):
+    return len(BULLET.findall(text))
 
 
 def test_default(capsys):
     main(["sphinxlint", "--list"])
     out, _err = capsys.readouterr()
-    assert out.count(BULLET) > 10
+    assert count_bullets(out) > 10
 
 
 def test_disable_all(capsys):
     main(["sphinxlint", "--disable", "all", "--list"])
     out, _err = capsys.readouterr()
-    assert out.count(BULLET) == 0
+    assert count_bullets(out) == 0
 
 
 def test_enable_all(capsys):
@@ -22,7 +27,7 @@ def test_enable_all(capsys):
     default_out, _err = capsys.readouterr()
     main(["sphinxlint", "--enable", "all", "--list"])
     all_out, _err = capsys.readouterr()
-    assert default_out.count(BULLET) < all_out.count(BULLET)
+    assert count_bullets(default_out) < count_bullets(all_out)
 
 
 def test_disable_one(capsys):
@@ -31,7 +36,7 @@ def test_disable_one(capsys):
     one_to_disable = choice(default_out.splitlines())[2:].split(":")[0]
     main(["sphinxlint", "--list", "--disable", one_to_disable])
     disabled_out, _err = capsys.readouterr()
-    assert default_out.count(BULLET) - 1 == disabled_out.count(BULLET)
+    assert count_bullets(default_out) - 1 == count_bullets(disabled_out)
 
 
 def test_enable_one(capsys):
@@ -43,4 +48,4 @@ def test_enable_one(capsys):
     one_to_enable = choice(list(not_enabled_by_default))[2:].split(":")[0]
     main(["sphinxlint", "--list", "--enable", one_to_enable])
     enabled_out, _err = capsys.readouterr()
-    assert default_out.count(BULLET) + 1 == enabled_out.count(BULLET)
+    assert count_bullets(default_out) + 1 == count_bullets(enabled_out)
