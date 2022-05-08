a5c3ba068087a4574934aae1e71bf6f1caef97ac
Julien Palard <julien@palard.fr>
2022-05-08 21:27:25 +0200

    Describe what the tests are testing.

--- tests/test_enable_disable.py
+++ tests/test_enable_disable.py
@@ -11,18 +11,24 @@ def count_bullets(text):
 
 
 def test_default(capsys):
+    """We know we have more than 10 checks (even more than 20), so make
+    sure `--list` show many of them.
+    """
     main(["sphinxlint", "--list"])
     out, _err = capsys.readouterr()
     assert count_bullets(out) > 10
 
 
 def test_disable_all(capsys):
+    """Checks that disabling all checks actually disable them all."""
     main(["sphinxlint", "--disable", "all", "--list"])
     out, _err = capsys.readouterr()
     assert count_bullets(out) == 0
 
 
 def test_enable_all(capsys):
+    """Some checks are disabled by default, so enabling them all should
+    give more checks than the default list."""
     main(["sphinxlint", "--list"])
     default_out, _err = capsys.readouterr()
     main(["sphinxlint", "--enable", "all", "--list"])
@@ -31,6 +37,8 @@ def test_enable_all(capsys):
 
 
 def test_disable_one(capsys):
+    """Disabling a single check from the default set (any of them) should
+    give one check less than the default set."""
     main(["sphinxlint", "--list"])
     default_out, _err = capsys.readouterr()
     one_to_disable = choice(default_out.splitlines())[2:].split(":")[0]
@@ -40,6 +48,8 @@ def test_disable_one(capsys):
 
 
 def test_enable_one(capsys):
+    """Enabling a single check not enabled by default should give one
+    check more than the default set."""
     main(["sphinxlint", "--list"])
     default_out, _err = capsys.readouterr()
     main(["sphinxlint", "--list", "--enable", "all"])
