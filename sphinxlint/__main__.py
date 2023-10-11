607e9916db773423a731e1579238367f59e3f77b
rtobar <rtobarc@gmail.com>
2023-10-11 10:19:51 +0800

    Update sphinxlint/__main__.py
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>

--- sphinxlint/__main__.py
+++ sphinxlint/__main__.py
@@ -60,7 +60,7 @@ def parse_args(argv=None):
 
     class StoreNumJobsAction(argparse.Action):
         def __call__(self, parser, namespace, values, option_string=None):
-            setattr(namespace, self.dest, StoreNumJobsAction._job_count(values))
+            setattr(namespace, self.dest, self._job_count(values))
         def _job_count(values):
             if values == "auto":
                 return os.cpu_count()
