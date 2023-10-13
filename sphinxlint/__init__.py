8c4a652d62b35445b6afb5cb6a470e9613ba3d3d
Ezio Melotti <ezio.melotti@gmail.com>
2023-10-13 12:44:32 +0200

    Hardcode package name.
    Co-authored-by: Hugo van Kemenade <hugovk@users.noreply.github.com>

--- sphinxlint/__init__.py
+++ sphinxlint/__init__.py
@@ -4,6 +4,6 @@ import importlib.metadata
 
 from sphinxlint.sphinxlint import check_file, check_text
 
-__version__ = importlib.metadata.version(__name__)
+__version__ = importlib.metadata.version("sphinx_lint")
 
 __all__ = ["check_text", "check_file"]
